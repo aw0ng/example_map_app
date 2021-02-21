ae002e475cb1cbd308eb1048938917ea0d657549
Anna Wong <76626238+aw0ng@users.noreply.github.com>
2021-02-21 11:39:02 -0500

    mapbox api integration

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Find Your Way Home</h1>
+    <div id="map"></div>
   </div>
 </template>
+
+<style>
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
+
+<script>
+/* global mapboxgl */
+
+export default {
+  mounted: function() {
+    this.setupMap();
+  },
+  methods: {
+    setupMap: function() {
+      mapboxgl.accessToken =
+        "pk.eyJ1IjoicGV0ZXJ4amFuZyIsImEiOiJjazljNjFmNW0wMWgyM2ZxdGJibW5zdDQ1In0.piikxmnHn9k0TnFYjNEoBQ";
+      var map = new mapboxgl.Map({
+        container: "map", // container id
+        style: "mapbox://styles/mapbox/outdoors-v11", // style URL
+        center: [-74.3599, 41.2565],
+        // startingposition [lng, lat]
+        zoom: 9, // starting zoom
+      });
+      console.log(map);
+    },
+  },
+};
+</script>
